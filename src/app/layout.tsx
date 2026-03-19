ffc37bb03de0e92d72e652cafd6d9b2bdea4b877
Roman Zelenka <romanzelenkajj@gmail.com>
2026-03-19 20:30:55 +0000

    Replace favicon with correct one from barmagazine.sk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon-32x32.png?v=4', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png?v=4', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png?v=5', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png?v=5', sizes: '16x16', type: 'image/png' },
     ],
-    shortcut: '/favicon.ico?v=4',
-    apple: '/apple-touch-icon.png?v=4',
+    shortcut: '/favicon.ico?v=5',
+    apple: '/apple-touch-icon.png?v=5',
   },
   openGraph: {
     title: 'BarMagazine',
